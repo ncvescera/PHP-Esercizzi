575aa66f18b29439b6521e773bab69a7a8c7dde3
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-12-27 17:48:21 +0100

    Commentato codice

--- cliente.php
+++ cliente.php
@@ -1,17 +1,21 @@
 <html>
 <head>
   <script>
-    var pizze = new Array();
+    var pizze = new Array(); //array che conterrà le pizze selezionate
 
+    //funzione per aggiungere un elemento all'array
     function add(valore){
       pizze.push(valore);
     }
 
+    //funzione per rimuovere un elemento dall'array
     function remove(valore){
       pizze.splice(pizze.indexOf(valore),1);
     }
 
+    //funzione per scegliere se eliminare o aggiungere un elemento
     function change(value){
+      //se l'elemento non esiste viene aggiunto
       if(pizze.indexOf(value) == -1){
         add(value);
       }
@@ -25,20 +29,22 @@
 </head>
 <body>
   <form>
-  <?php
-    session_start();
+    <?php
+      session_start();
 
-    $file = fopen("data.dat","r") or die("File inesistente!");
-    $i = 0;
-    while(!feof($file)){
-      $line = fgets($file);
-      if($line != null){
-        echo '<input type="checkbox"'."name=\"$i\" value=\"$line\" onchange=\"change(this.value)\">$line<br>";
-        $i++;
+      $file = fopen("data.dat","r") or die("File inesistente!"); //apertura del file con le pizze
+
+      //lettura del file e creazione delle checkbox
+      $i = 0;
+      while(!feof($file)){
+        $line = fgets($file);
+        if($line != null){
+          echo '<input type="checkbox"'."name=\"$i\" value=\"$line\" onchange=\"change(this.value)\">$line<br>";
+          $i++;
+        }
       }
-    }
-    fclose($file);
-  ?>
+      fclose($file);
+    ?>
   </form>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
-    session_start();
+    session_start(); //avvio della sessione
 
+    //inizializzazione delle variabili di sessione
     $_SESSION['nome']     = $_POST['nome'];
     $_SESSION['cognome']  = $_POST['cognome'];
     $_SESSION['tipo']     = $_POST['tipo'];
 
+    //scelta della pagina da caricare
     if ($_SESSION['tipo'] == "cliente") {
       header("Location:cliente.php");
     }
