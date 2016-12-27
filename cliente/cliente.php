a93320e2d0d1e9de73e907ca3ed031e6ea2a5691
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-12-27 18:59:48 +0100

    Iniziato sviluppo pagina cliente.php

--- cliente/cliente.php
+++ cliente/cliente.php
@@ -3,6 +3,21 @@
 
 </head>
 <body>
-  <?php session_start(); echo $_SESSION['tipo'];?>
+  <h1>Seleziona le pizze</h1>
+  <?php
+    session_start();
+
+    $file = fopen("../pizze.dat","r") or die("Impossibile aprire il file");
+
+    echo "<select name=\"pizze\" multiple>";
+    while(!feof($file)){
+      $line = fgets($file);
+      if($line != null){
+        echo "<option value=\"$line\">$line</option>";
+      }
+    }
+    fclose($file);
+    echo "</select>";
+  ?>
 </body>
 </html>
