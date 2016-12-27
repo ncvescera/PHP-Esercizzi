99b26eced1b77989853ff802bb699d5f865682ee
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-12-27 17:42:45 +0100

    Iniziato sviluppo cliente.php

--- cliente.php
+++ cliente.php
@@ -1,8 +1,44 @@
 <html>
 <head>
+  <script>
+    var pizze = new Array();
+
+    function add(valore){
+      pizze.push(valore);
+    }
+
+    function remove(valore){
+      pizze.splice(pizze.indexOf(valore),1);
+    }
+
+    function change(value){
+      if(pizze.indexOf(value) == -1){
+        add(value);
+      }
+      else {
+        remove(value);
+      }
+      alert(pizze);
+    }
+  </script>
 
 </head>
 <body>
-  <?php session_start(); echo $_SESSION['tipo']; ?>
+  <form>
+  <?php
+    session_start();
+
+    $file = fopen("data.dat","r") or die("File inesistente!");
+    $i = 0;
+    while(!feof($file)){
+      $line = fgets($file);
+      if($line != null){
+        echo '<input type="checkbox"'."name=\"$i\" value=\"$line\" onchange=\"change(this.value)\">$line<br>";
+        $i++;
+      }
+    }
+    fclose($file);
+  ?>
+  </form>
 </body>
 </html>
