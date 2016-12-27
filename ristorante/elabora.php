b663cc61505b3d9fcc1f1bd54cfce82c2340cd7a
Nicolò Vescera <nicolo.vescera@gmail.com>
2016-12-27 18:27:24 +0100

    Commentato codice

--- ristorante/elabora.php
+++ ristorante/elabora.php
@@ -2,12 +2,14 @@
   $array = json_decode($_GET['dati']); //decodifica dei dati in JSON
 
   $file = fopen("../pizze.dat","w") or die("Impossibile creare il file");
+
+  //scrittura dei dati sul file
   foreach ($array as $elem){
     fwrite($file,$elem);
   }
   fclose($file);
 
-  session_destroy();
+  session_destroy(); //distruzione della sessione
 
   echo "<h1>File scritto correttamente!</h1>";
   echo "Torna alla home <a href=\"/index.html\"><button>Home</button></a>";
